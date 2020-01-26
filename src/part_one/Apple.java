38ecd44f0a8e26530f2c5bee757a4b8ac092c5a6
정혜승 <mnb1@boneis.com>
2020-01-26 17:49:49 +0900

    initial commit

---
+++ src/part_one/Apple.java
@@ -0,0 +1,29 @@
+package part_one;
+
+public class Apple {
+	public int weight;
+	public String color;
+	
+	
+	public Apple(int weight, String color) {
+		super();
+		this.weight = weight;
+		this.color = color;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+	
+
+}
