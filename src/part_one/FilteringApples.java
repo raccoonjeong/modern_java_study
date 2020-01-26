79f9b61158762c1def9e3c3d0aa518a12ac05590
정혜승 <mnb1@boneis.com>
2020-01-26 19:17:46 +0900

    2020.01.26 [Chapter2] 익명클래스 사용

--- src/part_one/FilteringApples.java
+++ src/part_one/FilteringApples.java
@@ -20,16 +20,10 @@ public class FilteringApples {
 		List<Apple> inventory = Arrays.asList(new Apple(80, Color.GREEN),
 											  new Apple(155, Color.GREEN),
 											  new Apple(120, Color.RED));
-		List<Apple> heavyApples = filterApples(inventory, new AppleHeavyWeightPredicate());
-		List<Apple> greenApples = filterApples(inventory, new AppleGreenColorPredicate());
-		
-		System.out.println("무거운사과고르기...");
-		for(Apple apple : heavyApples) {
-			System.out.println(apple.toString());
-		}
-		System.out.println("초록사과고르기...");
-		for(Apple apple : greenApples) {
-			System.out.println(apple.toString());
-		}
+		List<Apple> redApples = filterApples(inventory, new ApplePredicate() {
+			public boolean test(Apple apple) {
+				return Color.RED.equals(apple.getColor());
+			}
+		}); // 익명클래스를 사용하여 즉석에서 구현했으나 너무 장황하다.
 	}
 }
